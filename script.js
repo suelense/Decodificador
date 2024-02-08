3da3a6d74eaf596e05739a69d251a6177bc22541
suelense <suelense@gmail.com>
2024-02-07 23:42:12 -0300

    responsividade

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
+function validarLetras(texto) {
+    var mensagem = texto;
+    var letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var validacao = false;
+    for (let i of mensagem) {
+        if (letras.includes(i)) {
+            var validacao = true;
+        }
+        else {
+            var validacao = false;
+            break;
+        }
+    }
+    return validacao;
+}
+
 function criptografarTexto() {
     var mensagemDescriptografada = document.getElementById("textoDescriptografado").value;
-    var mensagemCriptografada = mensagemDescriptografada.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
-    console.log(mensagemCriptografada);
-    document.getElementById("imagemMensagemNaoEncontrada").style.display = 'none';
-    document.getElementById("mensagemFinal").style.display = 'inline';
-    document.getElementById('mensagemFinal').innerText = mensagemCriptografada;
-    document.getElementById("botaoCopiar").style.display = 'inline';
+    var validacao = validarLetras(mensagemDescriptografada);
+    if (validacao == true) {
+        var mensagemCriptografada = mensagemDescriptografada.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+        document.getElementById("imagemMensagemNaoEncontrada").style.display = 'none';
+        document.getElementById("mensagemFinal").style.display = 'inline';
+        document.getElementById('mensagemFinal').innerText = mensagemCriptografada;
+        document.getElementById("botaoCopiar").style.display = 'inline';
+    }
+    else {
+        document.getElementById("mensagemAlerta").style.color = "#ff0000";
+        document.getElementById("mensagemAlerta").style.fontSize = "20px";
+    }
 }
+
 
 function descriptografarTexto() {
     var mensagemCriptografada = document.getElementById("textoDescriptografado").value;
     var mensagemDescriptografada = mensagemCriptografada.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-    console.log(mensagemDescriptografada);
     document.getElementById("imagemMensagemNaoEncontrada").style.display = 'none';
     document.getElementById("mensagemFinal").style.display = 'inline';
     document.getElementById('mensagemFinal').innerText = mensagemDescriptografada;
     document.getElementById("botaoCopiar").style.display = 'inline';
 }
 
+
 function copiarMensagem() {
     var mensagemCopiada = document.getElementById("mensagemFinal").value;
     navigator.clipboard.writeText(mensagemCopiada);
-    alert("Mensagem copiada!")
+    alert("Mensagem copiada!");
 }
-//verificar se tem maiusculas e outros caracters
